a5d186d86a7db8e9082aca4ec0e9185883e82ca5
Brayan Manzano <al221610173@gmail.com>
2025-07-21 13:54:35 -0600

    conatact floating

--- resources/views/frontend/components/contactFloating.blade.php
+++ resources/views/frontend/components/contactFloating.blade.php
@@ -38,7 +38,7 @@
         <button id="contactMainButton"
                 class="bg-gray-800 hover:bg-gray-700 text-white text-xs font-medium py-2 px-1 rounded-l-lg shadow-md flex flex-col items-center transition-all duration-300 focus:outline-none"
                 title="Opciones de contacto">
-            <span class="[writing-mode:vertical-rl] rotate-180 py-1">Contacto</span>
+            <span class="[writing-mode:vertical-rl] rotate-180 py-1 tracking-widest">Contacto</span>
             <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4 mt-1" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"/>
             </svg>
