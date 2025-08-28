1224aba52364617372b792e9177d57d457de7dbd
Brayan Manzano <al221610173@gmail.com>
2025-08-28 17:48:28 -0600

    cardContact Partial

--- resources/views/frontend/pages/mobiliario.blade.php
+++ resources/views/frontend/pages/mobiliario.blade.php
@@ -57,6 +57,7 @@
     </section>
     <section class="bg-white relative overflow-hidden" aria-labelledby="mobiliario-eventos">
         @include('frontend.partials.furnitureCards')
+        @include('frontend.partials.cardContact')
     </section>
 </main>
 @endsection
