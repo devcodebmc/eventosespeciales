5a27d8f5ad7bd1130cae88436c14b4e6ac2e3cc1
Brayan Manzano <al221610173@gmail.com>
2025-10-06 13:03:19 -0600

    open link

--- resources/views/frontend/partials/moments.blade.php
+++ resources/views/frontend/partials/moments.blade.php
@@ -49,8 +49,13 @@
                 <h4 class="text-xl sm:text-2xl font-secondary text-[#2A4044]">
                     {{ $moment->category->name }}
                 </h4>
-                <a href="{{ route('events.showEvent', $moment->slug) }}" class="text-gray-500 mt-1 text-sm sm:text-base">
+                <a href="{{ route('events.showEvent', $moment->slug) }}" 
+                    class="text-[#2A4044] mt-1 text-sm sm:text-base font-medium hover:text-[#4b8b97] transition-colors duration-300">
                     {{ $moment->title }}
+                    {{-- open link in new tab icon  --}}
+                    <svg class="inline-block ml-1 " xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path>
+                    </svg>
                 </a>
                 <div class="flex flex-wrap gap-2 mt-4">
                     @foreach ($moment->tags->take(3) as $tag)
