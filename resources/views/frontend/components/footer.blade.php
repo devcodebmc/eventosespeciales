f74a13640b26201301b0c2fda821b27d24c44353
Brayan Manzano <al221610173@gmail.com>
2025-10-07 15:48:30 -0600

    seo in footer

--- resources/views/frontend/components/footer.blade.php
+++ resources/views/frontend/components/footer.blade.php
@@ -140,7 +140,7 @@
                     viewBox="0 0 24 24"
                     fill="none"
                     stroke="currentColor"
-                    stroke-width="1"
+                    stroke-width="2"
                     stroke-linecap="round"
                     stroke-linejoin="round"
                     aria-hidden="true"
